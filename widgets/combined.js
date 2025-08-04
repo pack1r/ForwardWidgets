37e7c572948ffc3d00bc6a4df2bd8df7045d7578
pack1r <114560108+pack1r@users.noreply.github.com>
2025-08-04 23:52:02 +0800

    Update combined.js

--- widgets/combined.js
+++ widgets/combined.js
@@ -10,7 +10,7 @@ WidgetMetadata = {
   description: "聚合豆瓣、TMDB、IMDB和Bangumi的影视动画榜单",
   author: "阿米诺斯",
   site: "https://github.com/quantumultxx/ForwardWidgets",
-  version: "1.3.0",
+  version: "1.3.1",
   requiredVersion: "0.0.1",
   detailCacheDuration: 60,
   modules: [
@@ -2065,7 +2065,7 @@ async function fetchTmdbData(api, params) {
 }
 
 async function loadTmdbTrendingData() {
-    const response = await Widget.http.get("https://raw.githubusercontent.com/quantumultxx/ForwardWidgets/refs/heads/main/data/TMDB_Trending.json");
+    const response = await Widget.http.get("https://raw.githubusercontent.com/pack1r/ForwardWidgets/refs/heads/main/data/TMDB_Trending.json");
     return response.data;
 }
 
